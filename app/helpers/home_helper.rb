b27694f1835a7ed67d0d64c8761d665009a0cbb8
Derek Willis <dwillis@gmail.com>
2013-04-12 14:57:24 -0400

    consolidated donor/payee fields into one

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -14,6 +14,22 @@ module HomeHelper
     end
   end
   
+  def display_donor(item)
+    if item[:contributor_last_name]
+      "#{item[:contributor_last_name]}, #{item[:contributor_first_name]} #{item[:contributor_middle_name]}"
+    else
+      item[:contributor_organization_name]
+    end
+  end
+  
+  def display_payee(item)
+    if item[:payee_last_name]
+      "#{item[:payee_last_name]}, #{item[:payee_first_name]} #{item[:payee_middle_name]}"
+    else
+      item[:payee_organization_name]
+    end
+  end
+  
   def display_date(date)
     begin
       Date.parse(date)
